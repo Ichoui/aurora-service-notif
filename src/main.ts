aaa8b05ae2c478226535e65e8f128f6c73e58e24
Morgan Nicol <morganichoui@gmail.com>
2024-01-15 15:46:54 +0100

    Cron running

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cron from 'node-cron';
+import * as express from 'express';
+import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const server: express.Express = express();
+
+
+async function createNestServer(expressInstance: express.Express) {
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(expressInstance), {
+  });
+
   await app.listen(1994);
+
+  app.enableCors();
+  app.disable('x-powered-by');
+  app.disable('X-Powered-By');
+
+  const prefix = 'aurora';
+  app.setGlobalPrefix(prefix);
+
+  cron.schedule('* * * * *', () => {
+    console.log('running a task every minute ' , new Date());
+  });
+  return app.init();
 }
-bootstrap();
+
+
+createNestServer(server).then(e => console.log('Server is Running'));
